e62d7bf299fad5f5debc7c5001559bdb079027f9
Robin Hübner <profan@prfn.se>
2017-12-03 11:19:53 +0100

    removed unnecessary instruction

--- game.gd
+++ game.gd
@@ -1,7 +1,6 @@
 extends Node
 
 const Instructions = {
-	FORWARD = "^",
 	LEFT_TURN = "<",
 	RIGHT_TURN = ">",
 	U_TURN = "u"
--- ui.gd
+++ ui.gd
@@ -1,7 +1,6 @@
 extends Control
 
 # add instructions
-onready var go_fwd_btn = get_node("instr_container/instr_panels/instr_panel/instructions/go_fwd_btn")
 onready var turn_left_btn = get_node("instr_container/instr_panels/instr_panel/instructions/turn_left_btn")
 onready var turn_right_btn = get_node("instr_container/instr_panels/instr_panel/instructions/turn_right_btn")
 onready var uturn_btn = get_node("instr_container/instr_panels/instr_panel/instructions/uturn_btn")
@@ -21,7 +20,6 @@ onready var first_track = get_node("instr_container/instr_panels/tracks_panel/tr
 
 func _ready():
 	
-	go_fwd_btn.connect("pressed", self, "_on_go_fwd")
 	turn_left_btn.connect("pressed", self, "_on_turn_left")
 	turn_right_btn.connect("pressed", self, "_on_turn_right")
 	uturn_btn.connect("pressed", self, "_on_uturn")
@@ -38,9 +36,6 @@ func _process(delta):
 
 func _on_taxi_registered(taxi):
 	taxis_label.text = str(Game.get_taxi_count())
-
-func _on_go_fwd():
-	first_track.add_instruction("^")
 
 func _on_turn_left():
 	first_track.add_instruction("<")
