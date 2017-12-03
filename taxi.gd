c5d75182e7cf46e9bd54008170d95fea0332bdf0
Robin Hübner <profan@prfn.se>
2017-12-03 11:01:20 +0100

    shite

--- taxi.gd
+++ taxi.gd
@@ -1,10 +1,20 @@
 extends Sprite
 
-var taxi_id
-var instruction_id
-var instructions
-
 onready var body = get_node("body")
+
+enum Directions {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
+var taxi_id
+var taxi_dir
+
+# vm stuff
+var instructions
+var pc = 0
 
 func _ready():
 	Game.call_deferred("register_taxi", self)
