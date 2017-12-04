493e874ee567127aebb5874cffaf050da2340416
Robin Hübner <profan@prfn.se>
2017-12-04 11:32:42 +0100

    switch back to menu yes

--- level_select.gd
+++ level_select.gd
@@ -1,4 +1,11 @@
 extends Control
 
+onready var back_btn = get_node("bottom_buttons/back_button")
+
+var from_scene_name = false
+
 func _ready():
-	pass
+	back_btn.connect("pressed", self, "_on_back_press")
+
+func _on_back_press():
+	SceneSwitcher.goto_scene(from_scene_name)
