f0cf613d20215c23a8a7519673ac27950d9c7c1a
Robin Hübner <profan@prfn.se>
2017-12-04 11:09:57 +0100

    start, quit btn

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,14 @@
 extends Control
 
 onready var start_btn = get_node("start_game_btn")
+onready var quit_btn = get_node("quit_game_btn")
 
 func _ready():
-	pass
+	start_btn.connect("pressed", self, "_on_start_press")
+	quit_btn.connect("pressed", self, "_on_quit_press")
+	
+func _on_start_press():
+	SceneSwitcher.goto_scene("res://level_select.tscn")
+
+func _on_quit_press():
+	get_tree().quit()
